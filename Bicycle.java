b51d16a8b8439da23bb75862980fc78115438db5
leolujiayu <2015ljy@sina.com>
2018-02-28 09:06:14 +0800

    Bicycles

--- Bicycle.java
+++ Bicycle.java
@@ -1,7 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*Developer: Leo LUJIAYU Instructor: Mr.Daniel Date Feb 28th, 2018
+This program simulates bicycles.
  */
 package bicycle;
 
